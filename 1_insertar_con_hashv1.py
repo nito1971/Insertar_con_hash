9c72e6ada58c75eba1df957442be72dfc676ab3c
nito1971 <bgonpin@gmail.com>
2024-04-27 11:07:49 +0200

    correciones.

--- 1_insertar_con_hashv1.py
+++ 1_insertar_con_hashv1.py
@@ -1,6 +1,18 @@
 import os
+import hashlib
+
+
 ruta_origen = "/mnt/local/datos/Contras/Collection #2-#5 & Antipublic/Collection 1"
 ruta_destino = "/mnt/local/datos/Contras/TXT"
+extensiones_excluidas =["sql", "xlxs", "docx", "doc", "html"]
+
+
+def get_hash(input_string):
+    try:
+        return hashlib.sha384(input_string.encode()).hexdigest()
+    except:
+        return None
+
 
 
 def recorrer_directorio(ruta_directorio):
@@ -8,10 +20,13 @@ def recorrer_directorio(ruta_directorio):
             for root, dirs, files in os.walk(ruta_directorio):
                 try:
                     for file in files:
-                        ruta_completa = os.path.join(root, file)
-                        with open(ruta_completa, "r", encoding="latin1") as archivo:
-                            for linea in archivo:                                           
-                                contras_file.write(linea + "\n")                               
+                        if not file.endswith(tuple(extensiones_excluidas)):                            
+                            ruta_completa = os.path.join(root, file)
+                            with open(ruta_completa, "r", encoding="latin1") as archivo:
+                                for linea in archivo:
+                                    linea = get_hash(linea)
+                                    print(linea)                                          
+                                    #contras_file.write(linea + "\n")                               
                             
                 except FileNotFoundError as e:
                     print(f"Error al procesar archivo: {e}")
