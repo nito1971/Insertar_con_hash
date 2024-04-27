8803f407d9883e58cdbe6d038cc1c8caf7e57640
nito1971 <bgonpin@gmail.com>
2024-04-27 13:54:29 +0200

    comentado

--- 1_insertar_con_hashv1.py
+++ 1_insertar_con_hashv1.py
@@ -1,40 +1,43 @@
 import os
 import hashlib
 
-
+## Define the origin directory and the destination directory for writing output files.
 ruta_origen = "/mnt/local/datos/Contras/Collection #2-#5 & Antipublic/Collection 1"
 ruta_destino = "/mnt/local/datos/Contras/TXT"
-extensiones_excluidas =["sql", "xlxs", "docx", "doc", "html"]
-hashes = []
 
+## Define a list of file extensions to exclude from processing
+extensiones_excluidas = ["sql", "xlxs", "docx", "doc", "html"]
+hashes = []  # Initialize an empty list for storing hashes
 
 def get_hash(input_string):
+    """
+    Compute the SHA-384 hash of the input string.
+    If there is an error during the computation, return None.
+    :param input_string: The string to be hashed
+    :return: The computed hash (as a hexadecimal string) or None if an error occurs
+    """
     try:
         return hashlib.sha384(input_string.encode()).hexdigest()
     except:
         return None
 
 def recorrer_directorio(ruta_directorio):
-    with open(os.path.join(ruta_destino, "Collection_1.txt"), "a+", encoding="latin1") as contras_file:        
-            for root, dirs, files in os.walk(ruta_directorio):
-                try:
-                    for file in files:
-                        if not file.endswith(tuple(extensiones_excluidas)):                            
-                            ruta_completa = os.path.join(root, file)
-                            with open(ruta_completa, "r", encoding="latin1") as archivo:
-                                for linea in archivo:
-                                    linea = get_hash(linea)
-                                    if linea not in hashes:
-                                        hashes.append(linea)
-                                        contras_file.write(linea + "\n")
-                                        print(linea)
-                                    else:
-                                        print(f"Ya existe: {linea}")                                     
-                                                                  
-                            
-                except FileNotFoundError as e:
-                    print(f"Error al procesar archivo: {e}")
-                    
-      
-
-a = recorrer_directorio(ruta_origen)
+    """
+    Recursively traverse the directory and its subdirectories, processing files with certain extensions.
+    :param ruta_directorio: The path to the root directory
+    """
+    contras_file_path = os.path.join(ruta_destino, "Collection_1.txt")
+    with open(contras_file_path, "a+", encoding="latin1") as contras_file:
+        for root, dirs, files in os.walk(ruta_directorio):
+            try:
+                for file in files:
+                    if not file.endswith(tuple(extensiones_excluidas)):
+                        file_path = os.path.join(root, file)
+                        with open(file_path, "r", encoding="latin1") as archivo:
+                            lines = archivo.readlines()
+                            for line in lines:
+                                hashed_line = get_hash(line.decode())
+                                if hashed_line not in hashes:
+                                    hashes.append(hashed_line)
+                                    contras_file.write(f"{hashed_line}\n")
+                                    print(hashed_line  #
