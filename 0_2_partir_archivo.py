2f6b7c49d5e9814831a8c5bd16eb1350ffbe8677
nito1971 <bgonpin@gmail.com>
2024-05-01 15:04:11 +0200

    mejoras

--- 0_2_partir_archivo.py
+++ 0_2_partir_archivo.py
@@ -23,7 +23,7 @@ archivos_final = obtener_archivos(directorio_donde_buscar)
 
 
 for archivo in archivos_final:
-    limite_lineas = 1000
+    limite_lineas = 100
     try:        
         with open(archivo, encoding="latin-1") as f:
             for linea in f:
@@ -32,7 +32,7 @@ for archivo in archivos_final:
                     contador_linas += 1
                 elif contador_linas == limite_lineas:
                     nombre_archivo += 1
-                    with open(f"{ruta}/+{contador_archivos}.txt", "w") as f2:
+                    with open(f"{ruta}/305{contador_archivos}.txt", "w") as f2:
                         f2.writelines(lineas)
                         lineas = []
                         contador_linas = 0
